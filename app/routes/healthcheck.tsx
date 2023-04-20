4f693dd127d9a344dc4b4376ec237819cb4950b1
Nick Edgar <n.a.edgar@gmail.com>
2023-04-20 16:26:45 -0400

    Add info and logging to /healthcheck

--- app/routes/healthcheck.tsx
+++ app/routes/healthcheck.tsx
@@ -1,25 +1,29 @@
 // learn more: https://fly.io/docs/reference/configuration/#services-http_checks
 import type { LoaderArgs } from "@remix-run/node";
 
+import config from "~/config.server";
 import { prisma } from "~/db.server";
 
 export async function loader({ request }: LoaderArgs) {
-  const host =
-    request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
+  const host = request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
+
+  console.log("healthcheck config:", config);
+  console.log("healthcheck SESSION_SECRET:", process.env.SESSION_SECRET);
 
   try {
     const url = new URL("/", `http://${host}`);
     // if we can connect to the database and make a simple query
     // and make a HEAD request to ourselves, then we're good.
-    await Promise.all([
+    const [numUsers] = await Promise.all([
       prisma.user.count(),
       fetch(url.toString(), { method: "HEAD" }).then((r) => {
         if (!r.ok) return Promise.reject(r);
       }),
     ]);
-    return new Response("OK");
+    console.log("healthcheck ✅", { numUsers });
+    return new Response(["Status: OK", `Num users: ${numUsers}`].join("\n"));
   } catch (error: unknown) {
     console.log("healthcheck ❌", { error });
-    return new Response("ERROR", { status: 500 });
+    return new Response("Status: ERROR", { status: 500 });
   }
 }
